c1bfbdfe237e7389b647e60cabff64ae0dd92a40
Luis <hairchevere@gmail.com>
2023-01-09 09:46:51 -0600

    created department and role values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,19 @@
-INSERT INTO departments (name)
+INSERT INTO department (name)
 VALUES
 ('Executive'),
 ('Accounting & Finance'),
 ('Technology'),
 ('Marketing & Sales'),
 ('Human Resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('CEO', 300000, 1),
+('Account Manager', 200000, 2),
+('Data Analyst', 165000, 3),
+('Software developers', 125000, 3),
+('CFO', 250000, 2),
+('Sales Manager', 80000, 4),
+('HR Director', 150000, 3);
+
+INSERT INTO 
