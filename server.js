b391b1d72df2a39a2b110209d56a1ed4e9f934ef
Luis <hairchevere@gmail.com>
2023-01-10 14:28:38 -0600

    created connection const object

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const table = require('console.table');
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '28October!',
+        database: 'company_db',
+    }
+);
+
