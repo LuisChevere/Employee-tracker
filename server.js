1b4d2f355fd316ae9bf00c198043c8696032928a
Luis <hairchevere@gmail.com>
2023-01-10 14:32:33 -0600

    created inquirer prompt

--- server.js
+++ server.js
@@ -11,3 +11,23 @@ const connection = mysql.createConnection(
     }
 );
 
+function options() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'menu',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'Add Department',
+            'View All Roles',
+            'Add Role',
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'Delete Department',
+            'Delete Employee',
+            'Delete Role',
+            'Quit'
+        ]
+    })
+}
