e99c6a17d388627864f51be9241eb0d7325bbbf4
Luis <hairchevere@gmail.com>
2023-01-11 10:53:42 -0600

    started view all employees function

--- index.js
+++ index.js
@@ -155,3 +155,11 @@ function addRole() {
     });
 }
 
+function viewAllEmployees() {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.salary, role.title, department.name AS department, CONCAT(manager.first_name)," ", manager.last_name) As manager FROM employee
+
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON manager.id = employee.manager_id;`
+    
+}
