a215eaea821ab976b34ce6923b15ac5f6920725f
Luis <hairchevere@gmail.com>
2023-01-11 17:25:47 -0600

    added drop table for role and department

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,9 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
+DROP TABLE IF EXISTS department;
+drop TABLE if EXISTS role;
+
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
