fb27aa8dd26f61514e78dfd7a8d40c73be3c1864
Luis <hairchevere@gmail.com>
2023-01-11 18:50:35 -0600

    fixed INT in role table, removed comas in employee table

--- db/schema.sql
+++ db/schema.sql
@@ -13,14 +13,13 @@ CREATE TABLE department(
 );
 
 CREATE TABLE role(
-    id NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY(department_id)
-    REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
     ON DELETE CASCADE,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee(
@@ -29,10 +28,8 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY(role_id)
-    REFERENCES role(id),
-    FOREIGN KEY(manager_id),
-    REFERENCES employee(id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
     ON DELETE CASCADE,
     PRIMARY KEY(id)
 );
