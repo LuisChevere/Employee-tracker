8af872481501169f6f460c593274e5961fe3771d
Luis <hairchevere@gmail.com>
2023-01-11 18:51:06 -0600

    remove ; in employees and added ,

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,16 +13,15 @@ VALUES
 ('Data Analyst', 165000, 3),
 ('Software developers', 125000, 3),
 ('CFO', 250000, 2),
-('Sales Manager', 80000, 4),
-('HR Director', 150000, 3);
+('Sales Manager', 80000, 5),
+('HR Director', 150000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
 ('Michael', 'Scott', 1, null),
 ('Dwight', ' Schrute', 2, 1),
 ('Jim', 'Halpert', 3, 2),
-('Pam', 'Beesly', 5, 3),
-('Erin', 'Hannon', 4, null);
+('Pam', 'Beesly', 4, null),
 ('Ryan', 'Howard', 5, 3),
 ('Andy', 'Bernard', 5, 1),
 ('Kevin', 'Malone', 3, 4),
